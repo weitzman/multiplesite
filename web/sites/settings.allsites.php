a166235f1d436d31b06a32830a87a291c2b92944
Moshe Weitzman <weitzman@tejasa.com>
2016-07-08 14:36:29 -0400

    forgot settings.allsites.php

---
+++ web/sites/settings.allsites.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * $site_path is made available by Drupal core. Perhaps there is a better way.
+ */
+$sites_subdir = basename($site_path);
+
+$databases['default']['default'] = array (
+    'database' => $sites_subdir,
+    'username' => 'root',
+    'password' => '',
+    'prefix' => '',
+    'host' => '127.0.0.1',
+    'port' => '33067',
+    'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+    'driver' => 'mysql',
+);
+
+$settings['install_profile'] = 'standard';
+$config_directories['sync'] = '../config/'. $sites_subdir . '/sync';
