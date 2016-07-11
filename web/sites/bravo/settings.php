91e6d9b7a632e2f3e02390ef623d07d915ea2e1b
Moshe Weitzman <weitzman@tejasa.com>
2016-07-11 10:51:53 -0400

    Uncomment settings.local.php

--- web/sites/bravo/settings.php
+++ web/sites/bravo/settings.php
@@ -714,8 +714,9 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
  *
  * Keep this code block at the end of this file to take full effect.
  */
-# if (file_exists(__DIR__ . '/settings.local.php')) {
-#   include __DIR__ . '/settings.local.php';
-# }
 
 require __DIR__ . '/../settings.allsites.php';
+
+if (file_exists(__DIR__ . '/settings.local.php')) {
+ include __DIR__ . '/settings.local.php';
+}
